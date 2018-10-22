e635da8723d44c5e535ef9915fffa9c91a0308e1
swolecoder <swolecoder@gmail.com>
2018-10-22 13:04:20 -0400

    valid pafenthesis

---
+++ 20. Valid Parentheses/valid-parentheses.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  var stack = [],
+    brackets = {
+      '(': ')',
+      '{': '}',
+      '[': ']'
+    };
+
+  for (i = 0; i < s.length; i++) {
+    if (s[i] in brackets) {
+      stack.push(s[i]);
+    } else {
+      check = stack.pop();
+      if (brackets[check] != s[i]) {
+        return false;
+      }
+    }
+  }
+
+  // Stack should be empty by now.
+  if (stack.length != 0) return false;
+  return true;
+};
