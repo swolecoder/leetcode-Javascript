b5bf3ebe9715ea2e4ab262e7254c5de6c625ca5b
swolecoder <swolecoder@gmail.com>
2018-11-11 11:37:25 -0500

    largest number

--- 747. Largest Number At Least Twice of Others/largest-number-least-twice-other.js
+++ 747. Largest Number At Least Twice of Others/largest-number-least-twice-other.js
@@ -5,14 +5,14 @@
 var dominantIndex = function(nums) {
   let maxIndex = 0;
 
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0; i <= nums.length; i++) {
     if (nums[i] > nums[maxIndex]) {
       maxIndex = i;
     }
   }
 
   //     loop throigh the entire array
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0; i <= nums.length; i++) {
     if (maxIndex != i && nums[maxIndex] < 2 ** nums[i]) {
       return -1;
     }
